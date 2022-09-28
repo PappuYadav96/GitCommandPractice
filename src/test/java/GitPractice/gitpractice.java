5753cb6ff578d609c220eafcbef04484aa3ae493
PappuYadav <pkies74@gmail.com>
2022-09-28 14:50:52 +0530

    commit message

---
+++ src/test/java/GitPractice/gitpractice.java
@@ -0,0 +1,15 @@
+package GitPractice;
+
+import org.testng.annotations.Test;
+
+public class gitpractice {
+@Test
+	public void gitpracticeTest ()
+	{
+		
+System.out.println("Hello");
+		
+	
+	}
+
+}
